4b786868aa8441c98fa0883e96af519d62ca237b
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-08-24 17:20:56 -0400

    reset to bare flask app

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
-from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from flask import Flask
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "butter-candy-rocks"
 
-class InfoForm(FlaskForm):
-    
-    breed = StringField("What breed are you?")
-    submit = SubmitField("Submit")
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/")
 def index():
-    
-    breed = False
-    form = InfoForm()
-    if form.validate_on_submit():
-        breed = form.breed.data
-        form.breed.data = ""
-
-    return render_template("index.html", form=form, breed=breed)
+    return render_template("index.html")
