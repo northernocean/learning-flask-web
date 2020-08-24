ba69f37450a3c1cd59abe9bcecde10f7b1cca90b
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-08-24 11:54:18 -0400

    WTF forms basics

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
 app = Flask(__name__)
 
-@app.route("/")
+app.config["SECRET_KEY"] = "butter-candy-rocks"
+
+class InfoForm(FlaskForm):
+    
+    breed = StringField("What breed are you?")
+    submit = SubmitField("Submit")
+
+@app.route("/", methods=["GET","POST"])
 def index():
-    return render_template("index.html")
+    
+    breed = False
+    form = InfoForm()
+    if form.validate_on_submit():
+        breed = form.breed.data
+        form.breed.data = ""
 
-@app.route("/signup_form")
-def signup_form():
-    return render_template("signup.html")
-
-@app.route("/thank_you")
-def thank_you():
-    first = request.args.get("first")
-    last = request.args.get("last")
-    return render_template("thank_you.html", first=first, last=last)
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
-
-if __name__ == "__main__":
-    # Alternate launch configuration for catching errors with VSCode debugger
-    # https://blog.miguelgrinberg.com/post/setting-up-a-flask-application-in-visual-studio-code 
-    app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
+    return render_template("index.html", form=form, breed=breed)
