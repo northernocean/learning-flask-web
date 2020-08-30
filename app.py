dff2e72edcaf1201857623f7d661efafdb3d3721
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-08-30 10:01:18 -0400

    Flash alerts

--- app.py
+++ app.py
@@ -1,41 +1,28 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, flash, session, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import (StringField, BooleanField, DateTimeField, 
-    RadioField, SelectField, TextField, TextAreaField, SubmitField)
-from wtforms.validators import DataRequired
+from wtforms import (StringField, SubmitField)
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "butter-candy-maple"
 
 
-class MyForm(FlaskForm):
+class SimpleForm(FlaskForm):
 
-    breed = StringField("What breed are you?", validators=[DataRequired()])
-    neutered = BooleanField("Have you been neutered?")
-    mood = RadioField("Please choose your mood:", choices=[("1","happy"), ("2","hungry"), ("3", "excited")])
-    food_choice = SelectField("Pick your favorite food: ", choices=[("c","chicken"), ("b","beef"), ("f","fish")])
-    feedback = TextAreaField("Your Feedback:")
-    submit = SubmitField("Submit")
+    submit = SubmitField("Click Me")
 
 
 @app.route("/", methods=["GET","POST"])
 def index():
 
-    form = MyForm()
+    form = SimpleForm()
     
     if form.validate_on_submit():
-        session["breed"] = form.breed.data
-        session["neutered"] = form.neutered.data
-        session["mood"] = form.mood.data
-        session["food_choice"] = form.food_choice.data
-        session["feedback"] = form.feedback.data
-    
-        return redirect(url_for("thank_you"))
+        flash("You just clicked the button.")
+        return redirect(url_for("index"))
     
     return render_template("index.html", form=form)
 
 
-@app.route("/thank_you")
-def thank_you():
-    return render_template("thank_you.html")
+if __name__ == "__main__":
+    app.run()
