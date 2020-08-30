5a88370d9b5161697e9c906db3e7335dda8b0372
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-08-30 14:06:33 -0400

    Set up SQL Alchemy

--- app.py
+++ app.py
@@ -1,28 +1,16 @@
-from flask import Flask, render_template, flash, session, redirect, url_for
-from flask_wtf import FlaskForm
-from wtforms import (StringField, SubmitField)
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "butter-candy-maple"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 
-class SimpleForm(FlaskForm):
-
-    submit = SubmitField("Click Me")
 
 
-@app.route("/", methods=["GET","POST"])
-def index():
-
-    form = SimpleForm()
-    
-    if form.validate_on_submit():
-        flash("You just clicked the button.")
-        return redirect(url_for("index"))
-    
-    return render_template("index.html", form=form)
-
-
-if __name__ == "__main__":
-    app.run()
