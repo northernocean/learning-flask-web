045bdf7333e24a2ece9d2e927af06bfeb8807258
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-08-31 20:40:52 -0400

    Add Flask-Migrate dependency

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -10,6 +11,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////" + os.path.join(basedir, "d
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+Migrate(app, db)
 
 class Puppy(db.Model):
 
