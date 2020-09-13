687efd3edd4f6f5b74cfdfd64d00dee70b8a2f9e
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-09-12 20:37:40 -0400

    user views - part two

--- puppycompanyblog/users/picture_handler.py
+++ puppycompanyblog/users/picture_handler.py
@@ -3,7 +3,7 @@ from PIL import Image
 from flask import current_app
 
 
-def add_profile_image(image_to_upload: str, username: str):
+def add_profile_image(image_to_upload: str, username: str) -> str:
 
     filename, file_extension = os.path.splitext(image_to_upload.filename)
     new_filepath = os.path.join(current_app.root_path, "static",
--- puppycompanyblog/users/views.py
+++ puppycompanyblog/users/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request, Blueprint
-from puppycompanyblog.models import User
-from flask_login import login_user, logout_user
+from puppycompanyblog.models import User, BlogPost
+from flask_login import login_user, logout_user, login_required, current_user
 from puppycompanyblog import db
 from puppycompanyblog.users.forms import RegistrationForm, LoginForm, UpdateUserForm
 from puppycompanyblog.users.picture_handler import add_profile_image
@@ -10,13 +10,54 @@ users = Blueprint("users", __name__)
 # account
 # list of posts
 
+
+@users.route("/account", methods=["GET", "POST"])
+@login_required
+def account():
+
+    form = UpdateUserForm()
+
+    if form.validate_on_submit():
+
+        if form.image.data:
+            username = current_user.username
+            new_image_path = add_profile_image(form.image.data, username)
+            current_user.profile_image = new_image_path
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+
+        db.session.commit()
+
+        flash("User Account Updated.")
+        return redirect(url_for("core.index"))
+
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    profile_image = url_for("static", filename="profile_images/" + current_user.profile_image)
+    return render_template("account.html", profile_image=profile_image, form=form)
+
+
+@users.route("/<username>")
+def user_posts(username):
+
+    page = request.args.get("page", 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    blog_posts = BlogPost.query
+        .filter_by(author=user)
+        .order_by(BlogPost.date.desc())
+        .paginate(page=page, per_page=5)
+
+
 @users.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for("core.index"))
 
 
-@users.route("/register", methods=["GET","POST"])
+@users.route("/register", methods=["GET", "POST"])
 def register():
 
     form = RegistrationForm()
@@ -38,7 +79,7 @@ def register():
     return render_template("register.html", form=form)
 
 
-@users.route("/login", methods=["GET","POST"])
+@users.route("/login", methods=["GET", "POST"])
 def login():
 
     form = LoginForm()
