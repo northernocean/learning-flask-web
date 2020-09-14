a5ee60c44b685259e7b8c8aa365bc04b87ea65f4
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-09-14 07:49:43 -0400

    Hello World API

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-from puppycompanyblog import app
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+
+api = Api(app)
+
+
+class HelloWorld(Resource):
+
+    def get(self):
+        return {"Hello": "World"}
+
+
+api.add_resource(HelloWorld, "/")
 
 if(__name__ == "__main__"):
     app.run(debug=True)
